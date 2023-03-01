e827d50e779ddf3bcf14e8fc1d84efeb08e8f964
zacharytaylor-FS <ZJTaylor@student.fullsail.edu>
2023-03-01 04:57:55 -0500

    POST, GET by ID, PUT by ID, and DELETE by ID and fulfilled S/L/R design pattern

--- router/router.js
+++ router/router.js
@@ -11,4 +11,50 @@ router.get("/", (req, res, next) => {
 	});
 });
 
+router.post("/", (req, res, next) => {
+	res.status(201).json({
+		message: "Successful PUT!",
+		metadata: {
+			hostname: req.hostname,
+			method: req.method,
+		},
+	});
+});
+router.get("/:id", (req, res, next) => {
+	const id = req.params.id;
+	res.status(200).json({
+		message: "Successful - GET by ID",
+		id: id,
+		metadata: {
+			hostname: req.hostname,
+			method: req.method,
+		},
+	});
+});
+
+router.put("/:id", (req, res, next) => {
+	const id = req.params.id;
+	res.status(200).json({
+		message: "Successful - PUT by ID",
+		id: id,
+		metadata: {
+			hostname: req.hostname,
+			method: req.method,
+		},
+	});
+});
+
+router.delete("/:id", (req, res, next) => {
+	const id = req.params.id;
+	res.status(200).json({
+		message: "Successful - DELETE by ID",
+		id: id,
+		metadata: {
+			hostname: req.hostname,
+			method: req.method,
+		},
+	});
+});
+
+// router.delete();
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,3 @@
-/** NodeJS Follows
- * ? Design Pattern
- * * SLR - Server,Listener,Router Design
- * TODO create 3 seperate files
- * ? Module
- * * JS file
- */
 const http = require("http");
 const app = require("./app/app");
 require("dotenv").config();
