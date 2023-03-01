9aab2e9acde5d945d29075871b5139c5e660b2db
zacharytaylor-FS <ZJTaylor@student.fullsail.edu>
2023-03-01 10:52:00 -0500

    Update math.test

--- math/math.test.js
+++ math/math.test.js
@@ -1,20 +1,25 @@
+const { subtract } = require("./math");
 const mathOperations = require("./math");
 
 describe("Testing the math module", () => {
-	test("Adding 5 + 24 should return 29", () => {
+	//testing 5 + 24
+	test("test Adding 5 + 24 should return 29", () => {
 		let result = mathOperations.add(5, 24);
 		expect(result).toBe(29);
 	});
 
 	test("Subtract 150 - 50", () => {
-		expect(mathOperations.subtract(150, 50)).toBe(100);
+		let result = mathOperations.subtract(150, 50);
+		expect(result).toBe(100);
 	});
 
 	test("Multiply 5 * 5 should return 25", () => {
-		expect(mathOperations.multiply(5, 5)).toBe(25);
+		let result = mathOperations.multiply(5, 5);
+		expect(result).toBe(25);
 	});
 
 	test("Divide 30 / 5 should return 6", () => {
-		expect(mathOperations.divide(30, 5)).toBe(6);
+		let result = mathOperations.divide(30, 5);
+		expect(result).toBe(6);
 	});
 });
